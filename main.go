903ae3f96e31cc1ec26539bdd30dce4e65c37974
Snir Koppelman <snirkwork@gmail.com>
2022-07-29 19:33:16 +0300

    Created the views package

--- main.go
+++ main.go
@@ -2,28 +2,23 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/snirkop89/lenslocked/views"
 )
 
 func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html")
-	tpl, err := template.ParseFiles(filepath)
+	tpl, err := views.Parse(filepath)
 	if err != nil {
-		log.Printf("parsing template: %v", err)
+		log.Println(err)
 		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
 		return
 	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("exeuting template: %v", err)
-		http.Error(w, "Error executing the template", http.StatusInternalServerError)
-		return
-	}
+
+	tpl.Execute(w, nil)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
