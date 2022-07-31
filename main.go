f8b7511dc1679fae65d1ebec37613e9388649c11
Snir Koppelman <snirkwork@gmail.com>
2022-07-31 08:51:07 +0300

    embedding templates

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/snirkop89/lenslocked/controllers"
+	"github.com/snirkop89/lenslocked/templates"
 	"github.com/snirkop89/lenslocked/views"
 )
 
@@ -14,13 +14,13 @@ func main() {
 	r := chi.NewRouter()
 	// parse the templates
 	r.Get("/", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "home.html.tmpl")))))
+		views.Must(views.ParseFS(templates.FS, "home.html.tmpl"))))
 
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "contact.html.tmpl")))))
+		views.Must(views.ParseFS(templates.FS, "contact.html.tmpl"))))
 
 	r.Get("/faq", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "faq.html.tmpl")))))
+		views.Must(views.ParseFS(templates.FS, "faq.html.tmpl"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
