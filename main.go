2669282b6a76f8ea659f73b821999008034b9b8e
Snir Koppelman <snirkwork@gmail.com>
2022-07-31 08:19:56 +0300

    Using a StaticHandler in the controller package

--- main.go
+++ main.go
@@ -2,45 +2,35 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/snirkop89/lenslocked/controllers"
 	"github.com/snirkop89/lenslocked/views"
 )
 
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	tpl, err := views.Parse(filepath)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
-		return
-	}
-
-	tpl.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tmplPath := filepath.Join("templates", "home.html.tmpl")
-	executeTemplate(w, tmplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tmplPath := filepath.Join("templates", "contact.html.tmpl")
-	executeTemplate(w, tmplPath)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	tmplPath := filepath.Join("templates", "faq.html.tmpl")
-	executeTemplate(w, tmplPath)
-}
-
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+	// parse the templates
+	tpl, err := views.Parse(filepath.Join("templates", "home.html.tmpl"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.html.tmpl"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.html.tmpl"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
