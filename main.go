f7c2361d1a746f723a938d72ba9ec1df9259cf38
Snir Koppelman <snirkwork@gmail.com>
2022-08-05 11:06:13 +0300

    Created the users controller and moved the sign up page over it

--- main.go
+++ main.go
@@ -22,8 +22,10 @@ func main() {
 	r.Get("/faq", controllers.FAQ(
 		views.Must(views.ParseFS(templates.FS, "faq.html.tmpl", "tailwind.html.tmpl"))))
 
-	r.Get("/signup", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "signup.html.tmpl", "tailwind.html.tmpl"))))
+	usersC := controllers.Users{}
+	usersC.Templates.New = views.Must(
+		views.ParseFS(templates.FS, "signup.html.tmpl", "tailwind.html.tmpl"))
+	r.Get("/signup", usersC.New)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
