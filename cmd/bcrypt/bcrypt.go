87821557d33dd2df018f29302c85f3f4173bed1a
Snir Koppelman <snirkwork@gmail.com>
2022-09-23 09:38:48 +0300

    complete compare and hash functions

---
+++ cmd/bcrypt/bcrypt.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	for i, arg := range os.Args {
+		fmt.Println(i, arg)
+	}
+
+	switch os.Args[1] {
+	case "hash":
+		hash(os.Args[2])
+	case "compare":
+		compare(os.Args[2], os.Args[3])
+	default:
+		fmt.Printf("Invalid command %v\n", os.Args[1])
+	}
+}
+
+func hash(password string) {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Printf("error hashing: %v\n", password)
+		return
+	}
+	fmt.Println(string(hashedBytes))
+}
+
+func compare(password, hash string) {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		fmt.Println("invalid password", err)
+		return
+	}
+	fmt.Println("Password is valid")
+}
