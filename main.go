d5dac9099f0ef841f70d2cc529a536cdfd64ed54
Snir Koppelman <snirkwork@gmail.com>
2022-09-23 11:02:52 +0300

    add sign in view

--- main.go
+++ main.go
@@ -40,8 +40,11 @@ func main() {
 
 	usersC.Templates.New = views.Must(
 		views.ParseFS(templates.FS, "signup.html.tmpl", "tailwind.html.tmpl"))
+	usersC.Templates.SignIn = views.Must(
+		views.ParseFS(templates.FS, "signin.html.tmpl", "tailwind.html.tmpl"))
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
