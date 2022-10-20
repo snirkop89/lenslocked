35e1be608c96dc9a365ac542d829630e61bbc4bd
Snir Koppelman <snirkwork@gmail.com>
2022-10-20 20:41:38 +0300

    stubbing session service

--- models/session.go
+++ models/session.go
@@ -1,7 +1,28 @@
 package models
 
+import "database/sql"
+
 type Session struct {
-	ID        int
-	UserID    int
+	ID     int
+	UserID int
+	// Token is only set when creating a new session. when looking up a session
+	// this iwll be left empty, as we only store the hash of a session token
+	// in our database and we cannot reverse it into a raw token.
+	Token     string
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	// TODO: Create new session token
+	// TODO: Implement sessionService.Create
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	// TODO: Implement SessionService.User
+	return nil, nil
 }
