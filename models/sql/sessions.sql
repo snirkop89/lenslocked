c6ba52748996e7ccb2e5dbf6e06c441cccb05e6b
Snir Koppelman <snirkwork@gmail.com>
2023-02-04 16:24:48 +0200

    delete sql files

--- models/sql/sessions.sql
+++
@@ -1,7 +0,0 @@
--- a session belaongs to a user
--- a user has one session
-CREATE TABLE sessions (
-    id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE REFERENCES users (id) ON DELETE CASCADE,
-    token_hash TEXT UNIQUE NOT NULL
-);
--- models/sql/users.sql
+++
@@ -1,5 +0,0 @@
-create table users (
-    id SERIAL PRIMARY KEY,
-    email TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL
-);
