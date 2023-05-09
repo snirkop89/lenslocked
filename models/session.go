da25ac065a23dfcdb4fcd9a64d522eddffd34b93
Snir Koppelman <snirkwork@gmail.com>
2023-05-09 09:26:00 +0300

    Finish passwordReset create func

--- models/session.go
+++ models/session.go
@@ -59,14 +59,6 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 	SET token_hash = $2
 	RETURNING id;`, session.UserID, session.TokenHash)
 	err = row.Scan(&session.ID)
-	// if err == sql.ErrNoRows {
-	// 	row = ss.DB.QueryRow(`
-	// 	INSERT INTO
-	// 		sessions (user_id, token_hash)
-	// 	VALUES ($1, $2)
-	// 	RETURNING id;`, session.UserID, session.TokenHash)
-	// 	err = row.Scan(&session.ID)
-	// }
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
 	}
