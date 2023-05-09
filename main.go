9436ba2595575aea0fe3c06a9de017d0d7dfe775
Snir Koppelman <snirkwork@gmail.com>
2023-05-09 08:27:15 +0300

    Add reset password page

--- main.go
+++ main.go
@@ -110,6 +110,8 @@ func main() {
 		views.ParseFS(templates.FS, "forgot-pw.html.tmpl", "tailwind.html.tmpl"))
 	usersC.Templates.CheckYourEmail = views.Must(
 		views.ParseFS(templates.FS, "check-your-email.html.tmpl", "tailwind.html.tmpl"))
+	usersC.Templates.ResetPassword = views.Must(
+		views.ParseFS(templates.FS, "reset-pw.html.tmpl", "tailwind.html.tmpl"))
 
 	// Setup router and routes
 	r := chi.NewRouter()
@@ -128,6 +130,8 @@ func main() {
 	r.Post("/signout", usersC.ProcessSignOut)
 	r.Get("/forgot-pw", usersC.ForgotPassword)
 	r.Post("/forgot-pw", usersC.ProcessForgotPassword)
+	r.Get("/reset-pw", usersC.ResetPassword)
+	r.Post("/reset-pw", usersC.ProcessResetPassword)
 	r.Route("/users/me", func(r chi.Router) {
 		r.Use(umw.RequireUser)
 		r.Get("/", usersC.CurrentUser)
