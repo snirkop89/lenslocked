bf495af674a1eff33836774678d689cc62392d78
Snir Koppelman <snirkwork@gmail.com>
2023-05-12 13:43:35 +0300

    Add error package for public type

---
+++ errors/public.go
@@ -0,0 +1,24 @@
+package errors
+
+// Public wraps the original error with a new error that has a
+// Public() string method.
+func Public(err error, msg string) error {
+	return publicError{err, msg}
+}
+
+type publicError struct {
+	err error
+	msg string
+}
+
+func (pe publicError) Error() string {
+	return pe.err.Error()
+}
+
+func (pe publicError) Public() string {
+	return pe.msg
+}
+
+func (pe publicError) Unwrap() error {
+	return pe.err
+}
