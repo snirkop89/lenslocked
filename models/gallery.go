6060c6bcea1c5eab7ff367cbdd141a956f362f37
Snir Koppelman <snirkwork@gmail.com>
2023-07-28 17:47:17 +0300

    Add create image in service

--- models/gallery.go
+++ models/gallery.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path"
@@ -143,6 +144,27 @@ func (service *GalleryService) Image(galleryID int, filename string) (Image, err
 		GalleryID: galleryID,
 		Path:      imagePath,
 	}, nil
+}
+
+func (service *GalleryService) CreateImage(galleryID int, filename string, content io.Reader) error {
+	galleryDir := service.galleryDir(galleryID)
+	err := os.MkdirAll(galleryDir, 0755)
+	if err != nil {
+		return fmt.Errorf("creating gallery-%d image directory: %w", galleryID, err)
+	}
+	imagePath := path.Join(service.galleryDir(galleryID), filename)
+	dst, err := os.Create(imagePath)
+	if err != nil {
+		return fmt.Errorf("creating image file: %w", err)
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, content)
+	if err != nil {
+		return fmt.Errorf("creating image file: %w", err)
+	}
+
+	return nil
 }
 
 func (service *GalleryService) DeleteImage(galleryID int, filename string) error {
